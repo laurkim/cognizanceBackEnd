05d66a50ae23b64885f0c25456b6a0cc2fda8832
laurkim <laurakeem@gmail.com>
2017-11-16 05:51:08 -0500

    merge

--- index.js
+++ index.js
@@ -24,38 +24,36 @@ function initiateGameListener(json) {
   const startButton = document.getElementById("start-button");
   startButton.addEventListener("click", () => {
     generateCards(json);
-    timer();
+    // timer();
   })
 }
 
-function timer() {
-  let timeDiv = document.getElementsByClassName("timer-count");
-  let time = timeDiv[0].innerText;
-  time = "00:59";
-  timeDiv[0].innerText = time;
-  let seconds = parseInt(time.split(":")[1]);
-  let timer = setInterval(function() {
-    let timeDiv = document.getElementsByClassName("timer-count");
-    if (seconds === 0) {
-      console.log("timer is over");
-      clearInterval(timer)
-    } else if (seconds > 10) {
-      console.log("timer is not zero");
-        --seconds
-        timeDiv[0].innerText = `00:${seconds}`;
-    } else {
-      console.log("timer is less than 10");
-        --seconds
-        timeDiv[0].innerText = `00:0${seconds}`;
-    }
-  }, 1000)
-}
+// function timer() {
+//   let timeDiv = document.getElementsByClassName("timer-count");
+//   let time = timeDiv[0].innerText;
+//   time = "00:59";
+//   timeDiv[0].innerText = time;
+//   let seconds = parseInt(time.split(":")[1]);
+//   let timer = setInterval(function() {
+//     let timeDiv = document.getElementsByClassName("timer-count");
+//     if (seconds === 0) {
+//       console.log("timer is over");
+//       clearInterval(timer)
+//     } else if (seconds > 10) {
+//       console.log("timer is not zero");
+//         --seconds
+//         timeDiv[0].innerText = `00:${seconds}`;
+//     } else {
+//       console.log("timer is less than 10");
+//         --seconds
+//         timeDiv[0].innerText = `00:0${seconds}`;
+//     }
+//   }, 1000)
+// }
 
 function resetGame() {
   const resetButton = document.getElementsByClassName("game-reset");
   resetButton.addEventListener("click", () => {
-    let timeDiv = document.getElementsByClassName("timer-count");
-    timeDiv[0].innerText = "Timer";
     fetch("http://localhost:3000/users")
       .then(res => res.json())
       .then(json => json);
